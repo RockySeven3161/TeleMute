5a370b72038d369644bcca87d295a27e2225dad7
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2017-02-16 01:01:50 -0800

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,1 +1,24 @@
-...
+do
+
+local function run(msg, matches)
+if matches[1]=="ping" and is_sudo(msg) then 
+return  "انلاینم باباجونم"
+elseif matches[1]=="ping" and is_admin(msg) then 
+return  "🔹 Bot Is <b>Online</b> 🔹"
+elseif matches[1]=="ping" and is_owner(msg) then 
+return  "🔹 Bot Is <b>Online</b> 🔹"
+elseif matches[1]=="ping" and is_mod(msg) then 
+return  "🔹 Bot Is <b>Online</b> 🔹"
+else
+return  "🔹 Bot Is <b>Online</b> 🔹"
+end
+
+end
+
+return {
+  patterns = {
+    "^[!#/]ping$",
+    },
+  run = run
+}
+end
